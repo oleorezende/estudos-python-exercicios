47a299ddf05a05e92bfbc5733df8437ff6b1615c
Leonardo Rezende <engleonardorezende@gmail.com>
2026-02-26 21:49:26 -0300

    Add files via upload

---
+++ 01_exercicio.py
@@ -0,0 +1,9 @@
+# %%
+nome = input ("Qual o seu nome? ")
+print ("Olá,", nome,"tudo bem?")
+
+idade = input ("Qual a sua idade? ")
+print (idade)
+
+print ("Olá, Leo, bom saber que você tem", idade,"anos. Boas vindas!")
+# %%
---
+++ 02_raio.py
@@ -0,0 +1,31 @@
+# %%
+# ---------------------------------
+# Programa 1: Área e perímetro
+# ---------------------------------
+
+valor = float(input("Digite o raio em cm: "))
+print(f"A área é {3.14 * valor**2:.2f} cm² e o perímetro é {2 * 3.14 * valor:.2f} cm")
+
+
+# ---------------------------------
+# Programa 2: Soma de dois números
+# ---------------------------------
+
+valor1 = float(input("Digite um número: "))
+valor2 = float(input("Digite outro número: "))
+valor_total = valor1 + valor2
+
+print(f"A soma de {valor1} + {valor2} é {valor_total}")
+
+
+# ---------------------------------
+# Programa 3: Potência
+# ---------------------------------
+
+base = float(input("Digite a base: "))
+expoente = float(input("Digite o expoente: "))
+resultado = base ** expoente
+
+print(f"O resultado é {resultado}")
+
+# %%
